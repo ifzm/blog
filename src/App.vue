c567acaad4f21167739e6fc36f36e4456dfab5a7
ifzm <214123975@qq.com>
2017-01-05 22:58:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -42,18 +42,19 @@
         padding: 0;
         list-style: none;
     }
-
-    html, body {
+    
+    html,
+    body {
         letter-spacing: 0;
         font-family: "Helvetica Neue", Arial, "Microsoft YaHei", sans-serif;
         font-size: 1em;
         color: #333;
     }
-
+    
     .clearfix {
         zoom: 1;
     }
-
+    
     .clearfix:after {
         clear: both;
         content: '.';
@@ -62,72 +63,93 @@
         height: 0;
         visibility: hidden;
     }
-
+    
     a {
         outline-style: none;
         text-decoration: none;
         color: gray;
     }
-
+    
     a:hover {
         color: #333;
     }
-
+    
+     ::-webkit-scrollbar-track-piece {
+        background-color: #fff;
+        border-radius: 0;
+    }
+    
+     ::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+    }
+    
+     ::-webkit-scrollbar-thumb {
+        height: 50px;
+        background-color: #999;
+        border-radius: 4px;
+        outline: 2px solid #fff;
+        outline-offset: -2px;
+        border: 2px solid #fff;
+    }
+    
+     ::-webkit-scrollbar-thumb:hover {
+        height: 50px;
+        background-color: #9f9f9f;
+        border-radius: 4px;
+    }
+    
     main {
         padding: 10px;
     }
-
+    
     .view {
         margin: auto;
         position: absolute;
         left: 0;
         right: 0;
-        padding: 20px 0;
         max-width: 650px;
         transition: all .5s cubic-bezier(.55, 0, .1, 1);
     }
-
+    
     .err {
         color: #f44336;
     }
-
+    
     .slide-bottom-enter {
         opacity: 0;
         -webkit-transform: translate(0, 30px);
         transform: translate(0, 30px);
     }
-
+    
     .slide-bottom-leave-active {
         opacity: 0;
         -webkit-transform: translate(0, 30px);
         transform: translate(0, 30px);
     }
-
+    
     .about div {
         margin: 10px auto;
         text-align: center;
     }
-
+    
     .about .avatar {
         width: 90px;
         height: 90px;
     }
-
+    
     .about .avatar img {
         width: 100%;
         height: 100%;
         border-radius: 50%;
     }
-
+    
     .about .name {
-        font-size: 1.2em;
+        font-weight: bold;
+        color: #333;
     }
-
+    
     .about .motto {
         color: #888;
-    }
-
-    .centent {
-        padding: 30px 10px;
     }
 </style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -75,14 +75,14 @@
         position: relative;
         display: none;
     }
-
+    
     .block-title span {
         background: #fff;
         position: relative;
         z-index: 1;
         padding-right: 16px;
     }
-
+    
     .block-title::after {
         content: '';
         position: absolute;
@@ -92,27 +92,31 @@
         top: 11px;
         background: #f0f0f0;
     }
-
+    
+    .post-item {
+        padding: 30px 0;
+    }
+    
     .post-item .title {
         color: #333;
-        font-size: 1.2em;
+        font-size: 1.1em;
         font-weight: bold;
     }
-
+    
     .post-item .item-meta {
         font-size: 14px;
         color: #999;
         padding: 15px 0;
     }
-
+    
     .post-item .meta-left {
         float: left;
     }
-
+    
     .post-item .meta-right {
         float: right;
     }
-
+    
     .bull {
         margin: 0 2px;
     }
