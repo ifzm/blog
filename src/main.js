1cb958ea9240dcde0a220b9dd5d6dbd5cbeb7d8f
ifzm <devfzm@gmail.com>
2017-06-23 13:53:59 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,23 +2,22 @@ import Vue from 'vue'
 import router from './router'
 import App from './App'
 
+import axios from 'axios'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import '../static/base.css'
 NProgress.configure({ showSpinner: false })
 
-import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:4000/'
-axios.start = function() {
-    NProgress.start()
-    return this
+axios.start = function () {
+  NProgress.start()
+  return this
 }
 Promise.prototype.done = () => {
-    NProgress.done()
+  NProgress.done()
 }
 
-import '../static/base.css'
-
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount('#app')
