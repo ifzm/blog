50e43f200b58354870330d9dc133ef137919d75b
ifzm <devfzm@gmail.com>
2017-07-01 01:11:59 +0800

    🙋 router change

--- service/index.js
+++ service/index.js
@@ -3,17 +3,15 @@ global.Promise = require('bluebird')
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const { upload } = require('./utils')
 const routes = require('./routes')
 
 const app = express()
 
 require('moment').locale('zh-cn')
+app.use(methodOverride())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride())
 app.use(express.static('uploads'))
-
 app.all('*', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH')
@@ -21,26 +19,13 @@ app.all('*', (req, res, next) => {
   next()
 })
 
-app.post('/upload', upload.array('file'), (req, res, next) => {
-  let files = []
-
-  if (req.files) {
-    files = [].concat(req.files).map(file => ({
-      originalname: file.originalname,
-      filename: 'http://localhost:4000/' + file.filename
-    }))
-  }
-
-  res.json(files)
-})
-
 routes(app)
+
+app.listen(4000, () => {
+  console.log('service listen 4000...')
+})
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise ', p)
   console.log('reason: ', reason)
-})
-
-app.listen(4000, () => {
-  console.log('service listen 4000...')
 })
--- service/routes/index.js
+++ service/routes/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const mongoose = require('mongoose')
 const config = require('../config')
-const { glob } = require('../utils')
+const { glob, upload } = require('../utils')
 
 mongoose.Promise = Promise
 mongoose.connect(config.mongo.url)
@@ -13,6 +13,19 @@ db.once('open', () => { console.log(`${config.mongo.url} open success...`) })
 module.exports = async app => {
   const files = await glob(__dirname, '[^index]\\.js')
   files.forEach(file => require(path.resolve(__dirname, file))(app))
+
+  app.post('/upload', upload.array('file'), (req, res, next) => {
+    let files = []
+
+    if (req.files) {
+      files = [].concat(req.files).map(file => ({
+        originalname: file.originalname,
+        filename: 'http://localhost:4000/' + file.filename
+      }))
+    }
+
+    res.json(files)
+  })
 
   app.use((err, req, res, next) => {
     res.status(500).json({ error: err.message })
